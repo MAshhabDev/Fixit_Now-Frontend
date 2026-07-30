c01d13708567bda131bf8e9b155fa6c54a2b546c
Mahir4098 <mahirjr98@gmail.com>
2026-07-30 18:13:05 +0600

    feat: add midllware for check the role wised authorization and protect the pprivate route

---
+++ proxy.ts
@@ -0,0 +1,104 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { jwtUtils } from "./utils/jwt";
+import { getNewAccessToken } from "./service/refreshToken";
+import type { JwtPayload } from "jsonwebtoken";
+
+const AUTH_ROUTES = ["/login", "/register"];
+
+const PUBLIC_ROUTES = ["/", "/services"];
+// This function can be marked `async` if using `await` inside
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const cookieStore = await cookies();
+
+  let accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
+
+  let decodeAccessToken = accessToken
+    ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
+    : null;
+  const decodeRefreshToken = refreshToken
+    ? jwtUtils.verifyToken(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      )
+    : null;
+
+  // set new accesToken by use of refreshToken
+
+  if (!decodeAccessToken?.success && decodeRefreshToken?.success) {
+    const result = await getNewAccessToken();
+
+    if (result.success) {
+      const newAccessToken = result.data.accessToken;
+
+      cookieStore.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24,
+        sameSite: "lax",
+      });
+
+      accessToken = newAccessToken;
+      decodeAccessToken = jwtUtils.verifyToken(
+        accessToken!,
+        process.env.JWT_ACCESS_SECRET as string,
+      );
+    }
+  }
+
+  if (!decodeAccessToken?.success) {
+    cookieStore.delete("accessToken");
+  }
+
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  let userRole = null;
+
+  if (decodeAccessToken?.success && decodeAccessToken.data) {
+    userRole = (decodeAccessToken.data as JwtPayload).role;
+  }
+
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "CUSTOMER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+    if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    }
+    if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    }
+  }
+
+  if (pathname.startsWith("/dashboard") && userRole !== "CUSTOMER") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (
+    pathname.startsWith("/technician-dashboard") &&
+    userRole !== "TECHNICIAN"
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (pathname.startsWith("/admin-dashboard") && userRole !== "ADMIN") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)"],
+};
---
+++ service/refreshToken.ts
@@ -0,0 +1,27 @@
+import { cookies } from "next/headers";
+
+export const getNewAccessToken = async () => {
+  const cookieStore = await cookies();
+
+  const refreshToken = cookieStore.get("refreshToken");
+
+  if (!refreshToken) {
+    return {
+      success: false,
+      message: "Refresh token not found!",
+    };
+  }
+
+  const res = await fetch(`${process.env.BACKEND_API_URL}`, {
+    method: "POST",
+    headers: {
+      Cookie: `refreshToken=${refreshToken}`,
+    },
+
+    cache: "no-cache",
+  });
+
+  const result = await res.json();
+
+  return result;
+};
